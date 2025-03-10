9a315470f0604770c5a830313c0cb83d82701ba7
pedromondek <phmondek@gmail.com>
2025-03-10 19:05:44 -0300

    :truck: chore: Renamed and corrected for better understanding

---
+++ back-end/src/routes/http/dto/controller.dto.ts
@@ -0,0 +1,56 @@
+export interface IUserBody {
+  username: string;
+  password: string;
+  profileImage?: string | null;
+}
+
+export interface IChangeUser {
+  username?: string;
+  password?: string;
+  profileImage?: string;
+}
+
+export interface ISearchUser {
+  page?: number;
+  pageSize?: number;
+  username?: string;
+}
+
+export interface IResponseUserSearch {
+  id: number;
+  username: string;
+  profileImage?: string | null;
+}
+
+// Dto do chat
+
+export interface IChat {
+  id: number;
+  title?: string | null;
+  createdAt: Date;
+  isGroup: boolean;
+  groupImage: string | null;
+  chattings: UserChatDto[];
+  messages: MessageDto[];
+}
+
+export interface UserChatDto {
+  userId: number;
+  chatId: number;
+  user: UserChatting;
+}
+
+export interface UserChatting {
+  id: number;
+  username: string;
+  profileImage: string | null;
+}
+
+export interface MessageDto {
+  id: number;
+  authorId: number;
+  chatId: number;
+  content: string;
+  timestamp: Date;
+  viewed: boolean;
+}
