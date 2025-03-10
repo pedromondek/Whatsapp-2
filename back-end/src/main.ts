3efc849155cc1144f631cfda72716cdda4061ac6
pedromondek <phmondek@gmail.com>
2025-03-10 19:20:14 -0300

    :wrench: chore: Changed the way dependencies and servers are consumed

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -2,21 +2,25 @@ import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import http from 'http';
-import * as services from './routes/services.js';
-import * as socket from './routes/ws.js';
+import * as services from './routes/http/controller.controller.js';
+import * as socket from './routes/sockets/ws.js';
 
 dotenv.config();
 const app = express();
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 app.use(express.json());
 
 const wsServer = http.createServer(app);
 
 app.use('/services', services.servicesModule(prisma));
-socket.wsModule(wsServer);
 
-const PORT = process.env.PORT_WS ?? 9000;
+const io = socket.wsModule(wsServer, prisma);
+const PORT_WS = Number(process.env.PORT_WS) ?? 8888;
+
+io.listen(PORT_WS);
+
+const PORT = process.env.PORT_APP ?? 9000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta: ${PORT}`);
 });
