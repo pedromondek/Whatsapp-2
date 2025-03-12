bc078707f3f3a7a13690bd5de352cf59cc479916
pedromondek <phmondek@gmail.com>
2025-03-12 19:02:25 -0300

    :recycle: refactor: Change socket for class and now chat its functional

--- back-end/src/routes/sockets/ws.ts
+++ back-end/src/routes/sockets/ws.ts
@@ -1,52 +1,80 @@
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import http from 'http';
 import { PrismaClient } from '@prisma/client';
 import { HttpMethods } from '../http/http.services';
 
-export const wsModule = (server: http.Server, prisma: PrismaClient) => {
-  const servicesMethods = new HttpMethods(prisma);
+export class WebSocketService {
+  private io: Server;
+  private servicesMethods: HttpMethods;
+  private userConnections: Map<string, string[]>;
 
-  const io = new Server(server, {
-    cors: {
-      origin: '*',
-      methods: ['GET', 'POST'],
-    },
-  });
+  constructor(server: http.Server, prisma: PrismaClient) {
+    this.servicesMethods = new HttpMethods(prisma);
+    this.userConnections = new Map();
 
-  const userConnections = new Map();
-  let socketUserId = '';
+    this.io = new Server(server, {
+      cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+      },
+    });
 
-  io.on('connection', (socket) => {
-    // console.log(`Novo cliente conectado com socket.id: ${socket.id}`);
+    this.socketEvents();
+  }
 
-    socket.on('registerConnection', ({ userId }) => {
-      socketUserId = userId;
+  private async socketEvents(): Promise<void> {
+    this.io.on('connection', (socket: Socket) => {
+      let socketUserId = '';
 
-      if (userConnections.has(userId)) {
-        userConnections.get(userId).push(socket.id);
-      } else {
-        userConnections.set(userId, [socket.id]);
+      socket.on('registerConnection', ({ userId }) => this.registerConnection(socket, userId));
+
+      socket.on('message', async ({ userId, chatId, content }) => this.newMessage(socket, userId, chatId, content));
+
+      socket.on('disconnect', () => this.registerDisconnect(socket, socketUserId));
+
+      this.joinChats(socket);
+    });
+  }
+
+  private async joinChats(socket: Socket): Promise<void> {
+    socket.on('joinChats', ({ chatsId }) => {
+      chatsId.forEach((chatId: number) => {
+        socket.join(`chat_${chatId}`);
+        console.log(`Usuário socket.id: ${socket.id}, entrou na sala: chat_${chatId}`);
+      });
+    });
+  }
+
+  private async registerConnection(socket: Socket, userId: string) {
+    if (this.userConnections.has(userId)) {
+      this.userConnections.get(userId)?.push(socket.id);
+    } else {
+      this.userConnections.set(userId, [socket.id]);
+    }
+
+    console.log(`Usuário ${userId} registrado com socket.id: ${socket.id}`);
+  }
+
+  private async newMessage(socket: Socket, userId: number, chatId: number, content: string) {
+    const message = await this.servicesMethods.createMessage(userId, chatId, content);
+
+    console.log(`Mensagem salva no BD:`, message);
+
+    this.io.to(`chat_${chatId}`).emit('newMessage', message);
+  }
+
+  private async registerDisconnect(socket: Socket, userId: string) {
+    console.log(`Cliente desconectado: ${socket.id}`);
+
+    this.userConnections.forEach((sockets, uid) => {
+      this.userConnections.set(
+        uid,
+        sockets.filter((id) => id !== socket.id),
+      );
+
+      if (this.userConnections.get(uid)?.length === 0) {
+        this.userConnections.delete(uid);
       }
-
-      console.log(`Usuário ${userId} registrado com socket.id: ${socket.id}`);
     });
-
-    // console.log(`Novo cliente conectado: ${socket.id}`);
-
-    socket.on('message', async ({ chatId, content }) => {
-      const userId = socketUserId;
-
-      // const message = servicesModule;
-      // io.to(`chat_${chatId}`).emit('newMessage', content);
-      console.log(`Mensagem recebida de ${userId}: ${content}`);
-      // io.emit('message', { userId: socket.id, content });
-    });
-
-    socket.on('disconnect', () => {
-      console.log(`Cliente desconectado: ${socket.id}`);
-      userConnections.delete(socket.id);
-    });
-  });
-
-  return io;
-};
+  }
+}
